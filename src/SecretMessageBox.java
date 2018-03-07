c5ebc901da2c1f71b007a35448228d4e39273287
league <league@wts9.attlocal.net>
2018-03-06 16:58:52 -0800

    robots in space, secret message box, the riddler, x geeks

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -16,7 +16,19 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
-	
+	public static void main(String[] args) {
+		String password = JOptionPane.showInputDialog("enter a password");
+		String code = JOptionPane.showInputDialog("what's the code");
+		String word = JOptionPane.showInputDialog("what's the password");
+		if(word.equals (password))
+		{
+			JOptionPane.showMessageDialog(null, code);
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, "INTRUDER");
+		}
+	}
 	// 1. Set a password in a String variable
 	
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
--- src/XGeeks.java
+++ src/XGeeks.java
@@ -15,11 +15,16 @@ public class XGeeks {
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
+String superpower =/*assign*/ "flying";
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
+String name = JOptionPane.showInputDialog("what's your name");
 
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+if(name.equals("bob"))
+{
+	JOptionPane.showMessageDialog(null, superpower);
+}
 	}
 }
 
